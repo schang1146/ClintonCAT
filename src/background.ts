146a87b32d5ed7ce2c7208e1a4fb1cc109547fa6
schang1146 <20074034+schang1146@users.noreply.github.com>
2025-06-03 18:44:57 -0700

    WIP

--- src/background.ts
+++ src/background.ts
@@ -4,7 +4,7 @@ import browser, { Runtime } from 'webextension-polyfill';
 
 const main = new Main();
 
-browser.runtime.onMessage.addListener(() => {
+browser.runtime.onInstalled.addListener(() => {
     main.onBrowserExtensionInstalled();
 });
 
@@ -12,3 +12,13 @@ browser.runtime.onMessage.addListener((message, sender: Runtime.MessageSender, s
     main.onBrowserExtensionMessage(message as IMainMessage, sender, sendResponse);
     return true;
 });
+
+browser.tabs.onActivated.addListener((activeInfo) => {
+    browser.tabs.get(activeInfo.tabId).then((tabInfo) => {
+        console.log('fffff: onActivated', { activeInfo, tabInfo });
+    });
+});
+browser.tabs.onUpdated.addListener(
+    (tabId, changeInfo, tabInfo) => console.log('fffff: onUpdated', { tabId, changeInfo, tabInfo }),
+    { properties: ['url'] }
+);
